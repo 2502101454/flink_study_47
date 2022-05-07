a0cf32ff342fd16c775e67ce60983f42429e5498
wangzeng <2502101454@qq.com>
2022-05-07 09:24:36 +0800

    update

---
+++ src/main/java/com/atguigu/chater05/ClickSource.java
@@ -0,0 +1,39 @@
+package com.atguigu.chater05;
+
+import org.apache.flink.streaming.api.functions.source.SourceFunction;
+
+import java.util.Calendar;
+import java.util.Random;
+
+/**
+ * @author zengwang
+ * @create 2022-05-05 23:23
+ * @desc:
+ */
+public class ClickSource implements SourceFunction<Event> {
+    // 声明一个标志位
+    private Boolean running = true;
+
+    @Override
+    public void run(SourceContext<Event> ctx) throws Exception {
+        // 随机生成数据
+        Random random = new Random();
+        // 定义字段选取的数据集
+        String[] users = {"Mary", "Alice", "Bob", "Cary"};
+        String[] urls = {"./home", "./cart", "./fav", "./prod?id=100", "./prod?id=10"};
+
+        // 循环生成
+        while (running) {
+            String user = users[random.nextInt(users.length)];
+            String url = urls[random.nextInt(urls.length)];
+            long timestamp = Calendar.getInstance().getTimeInMillis();
+            ctx.collect(new Event(user, url, timestamp));
+            Thread.sleep(1000L);
+        }
+    }
+
+    @Override
+    public void cancel() {
+        running = false;
+    }
+}
---
+++ src/main/java/com/atguigu/chater05/SourceCustomTest.java
@@ -0,0 +1,43 @@
+package com.atguigu.chater05;
+
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.source.ParallelSourceFunction;
+
+import java.util.Random;
+
+/**
+ * @author zengwang
+ * @create 2022-05-05 23:22
+ * @desc:
+ */
+public class SourceCustomTest {
+    public static void main(String[] args) throws Exception {
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        env.setParallelism(4);
+        // 有了自定义的source function，调用addSource方法
+        // DataStreamSource<Event> customStream = env.addSource(new ClickSource());
+        DataStreamSource<Integer> customStream = env.addSource(new CustomSource()).setParallelism(2);
+        // print算子，并行度取env的，为4
+        customStream.print();
+        env.execute();
+    }
+
+    public static class CustomSource implements ParallelSourceFunction<Integer> {
+
+        private boolean running = true;
+        private Random random = new Random();
+
+        @Override
+        public void run(SourceContext<Integer> ctx) throws Exception {
+            while (running) {
+                ctx.collect(random.nextInt());
+            }
+        }
+
+        @Override
+        public void cancel() {
+            running = false;
+        }
+    }
+}
